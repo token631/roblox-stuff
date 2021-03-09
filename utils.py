df7196c462eced4c103905febc26b3ca5731c9c1
token631 <67794936+token631@users.noreply.github.com>
2021-03-09 18:30:54 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 """
-Dictionary containing different AssetTypes and what they are.
+Dictionary containing different AssetTypes and what they are
+SOURCE: https://developer.roblox.com/en-us/api-reference/enum/AssetType 
 """
 
-AssetTypes = {
+asset_types = {
   1 : "Image",
   2 : "TeeShirt",
   3 : "Audio",
@@ -49,3 +50,19 @@ AssetTypes = {
   61 : "EmoteAnimation",
   62 : "Video"
 }	
+
+"""
+URL used to get a user's group data
+SOURCE: https://groups.roblox.com/docs#!/Groups/get_v2_users_userId_groups_roles
+"""
+
+user_id = 123
+group_url = f"https://groups.roblox.com/v2/users/{user_id}/groups/roles"
+
+"""
+URL used to get asset data
+SOURCE: https://api.roblox.com/docs#Marketplace
+"""
+
+asset_id = 123
+asset_info_url - f"https://api.roblox.com/Marketplace/ProductInfo?assetId={asset_id}"
